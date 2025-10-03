8b0cfd7b3bfb23c58edc5b17045cbc21fd1d2f4f
elbren <elbrenantonio@sksu.edu.ph>
2025-10-03 21:09:15 +0800

    feature/11

--- app/Http/Controllers/Api/Admin/SlaughterPrivate/SLPrivate.php
+++ app/Http/Controllers/Api/Admin/SlaughterPrivate/SLPrivate.php
@@ -37,4 +37,70 @@ class SLPrivate extends BaseController
         ]);
     }
 
+    public function index()
+    {
+        $transactions = SlaughterPrivate::orderBy('created_at', 'desc')->get();
+
+        return response()->json([
+            'success' => true,
+            'data'    => $transactions
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $transaction = SlaughterPrivate::find($id);
+
+        if (!$transaction) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Transaction not found.'
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'date'   => 'required|date',
+            'or_no'  => 'required|unique:slaughter_privates,or_no,' . $id,
+            'agency' => 'required|string',
+            'owner'  => 'required|string',
+            'small_heads' => 'nullable|integer|min:0',
+            'large_heads' => 'nullable|integer|min:0',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'errors'  => $validator->errors()
+            ], 422);
+        }
+
+        $transaction->update($request->all());
+
+        return response()->json([
+            'success' => true,
+            'data'    => $transaction,
+            'message' => 'Transaction updated successfully.'
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $transaction = SlaughterPrivate::find($id);
+
+        if (!$transaction) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Transaction not found.'
+            ], 404);
+        }
+
+        $transaction->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Transaction deleted successfully.'
+        ]);
+    }
+
+
 }
